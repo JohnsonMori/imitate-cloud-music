941b6ff8c99f36663e2ee9870a543b3c7c26c74e
JohnsonMori <960612296@qq.com>
2020-03-06 22:22:27 +0800

    carousel module

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
+import { Banner } from 'src/app/services/data-types/common.typs';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,11 @@ import { HomeService } from 'src/app/services/home.service';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  banners: Banner[]
 
   constructor(private homeserve: HomeService) {
     this.homeserve.getBanners().subscribe(banners => {
-      console.log('banners :', banners)
+      this.banners = banners
     })
    }
 
