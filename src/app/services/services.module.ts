b5f8f7524a14e20b97c2b55fb2342661cdec5db5
JohnsonMori <960612296@qq.com>
2020-03-06 17:42:15 +0800

    get banner data

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 
-
+export const API_CONFIG = new InjectionToken('ApiConfigToken');
 
 @NgModule({
   declarations: [],
   imports: [
+  ],
+  providers: [
+    { provide: API_CONFIG, useValue: 'http://musicapi.leanapp.cn/' }
   ]
 })
 export class ServicesModule { }
