68ba6e397f3e1d579b9878c440093b86d99fd92f
JohnsonMori <960612296@qq.com>
2020-03-06 15:26:35 +0800

    modular design

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    NgZorroAntdModule,
+    FormsModule,
+    CommonModule
+  ],
+  exports: [
+    NgZorroAntdModule,
+    FormsModule,
+    CommonModule
+  ]
+})
+export class ShareModule { }
