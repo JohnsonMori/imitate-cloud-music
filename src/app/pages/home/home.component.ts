378affdf1959963a6b47b19b67635ec32d3dde54
JohnsonMori <960612296@qq.com>
2020-03-07 13:32:58 +0800

    carousel module

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
 import { Banner } from 'src/app/services/data-types/common.typs';
+import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,10 @@ import { Banner } from 'src/app/services/data-types/common.typs';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  carouselActiveIndex = 0
   banners: Banner[]
+
+  @ViewChild(NzCarouselComponent, {static: true}) private nzCarousel: NzCarouselComponent
 
   constructor(private homeserve: HomeService) {
     this.homeserve.getBanners().subscribe(banners => {
@@ -17,6 +21,14 @@ export class HomeComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  onBeforeChange({ to }) {
+    this.carouselActiveIndex = to
+  }
+
+  onChangeSlide(type: 'pre' | 'next') {
+    this.nzCarousel[type]()
   }
 
 }
