f8a013f9a3cb6a44ee47bd2d521536d45be75ec4
JohnsonMori <960612296@qq.com>
2020-03-12 21:24:49 +0800

    slider component

--- src/app/share/wy-ui/wy-player/wy-player.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WyPlayerComponent implements OnInit {
   sliderValue = 35;
+  wyOffset = 70;
   constructor() { }
 
   ngOnInit(): void {
