7a0eb7aee163e9cd28446e92094e1dbb0906cf58
JohnsonMori <960612296@qq.com>
2020-03-21 20:02:33 +0800

    play mode

--- src/app/share/wy-ui/wy-player/wy-player.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player.component.ts
@@ -5,9 +5,21 @@ import { getSongList, getPlayList, getCurrentIndex, getPlayer, getPlayMode, getC
 import { Song } from 'src/app/services/data-types/common.typs';
 import { PlayState } from 'src/app/store/reducers/player.reducer';
 import { PlayMode } from './player-type';
-import { SetCurrentIndex } from 'src/app/store/actions/player.actions';
+import { SetCurrentIndex, SetPlayMode, SetPlayList } from 'src/app/store/actions/player.actions';
 import { Subscription, fromEvent } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
+import { shuffle } from 'src/app/utils/array';
+
+const modeTypes: PlayMode[] = [{
+  type: 'loop',
+  label: '循环'
+}, {
+  type: 'random',
+  label: '随机'
+}, {
+  type: 'singleLoop',
+  label: '单曲循环'
+}];
 
 @Component({
   selector: 'app-wy-player',
@@ -40,6 +52,10 @@ export class WyPlayerComponent implements OnInit {
 
   // 是否点击的是音量面板本身
   selfClick = false;
+
+  // 当前模式
+  currentMode: PlayMode;
+  modeCount = 0;
 
   private winClick: Subscription;
 
@@ -92,6 +108,15 @@ export class WyPlayerComponent implements OnInit {
 
   private watchPlayMode(mode: PlayMode) {
     console.log('mode: ', mode);
+    this.currentMode = mode;
+    if (this.songList) {
+      let list = this.songList.slice();
+      if (mode.type === 'random') {
+        list = shuffle(this.songList);
+        this.updateCurrentIndex(list, this.currentSong);
+         this.store$.dispatch(SetPlayList({ playList: list }));
+      }
+    }
   }
 
   private watchCurrentSong(song: Song) {
@@ -100,6 +125,16 @@ export class WyPlayerComponent implements OnInit {
       this.duration = song.dt / 1000;
       console.log('song: ', song);
     }
+  }
+
+  private updateCurrentIndex(list: Song[], song: Song) {
+    const newIndex = list.findIndex(item => item.id === song.id);
+    this.store$.dispatch(SetCurrentIndex({ currentIndex: newIndex }));
+  }
+
+  // 改变模式
+  changeMode() {
+    this.store$.dispatch(SetPlayMode({ playMode: modeTypes[++this.modeCount % 3] }));
   }
 
   onPercentChange(per: number) {
@@ -171,7 +206,7 @@ export class WyPlayerComponent implements OnInit {
     if (this.playList.length === 1) {
       this.loop();
     } else {
-      const newIndex = index <= 0 ? this.playList.length -1 : index;
+      const newIndex = index < 0 ? this.playList.length -1 : index;
       this.updateIndex(newIndex);
     }
   }
@@ -184,6 +219,16 @@ export class WyPlayerComponent implements OnInit {
     } else {
       const newIndex = index >= this.playList.length ? 0 : index;
       this.updateIndex(newIndex);
+    }
+  }
+
+  // 播放结束
+  onEnded() {
+    this.playing = false;
+    if (this.currentMode.type === 'singleLoop') {
+      this.loop();
+    } else {
+      this.onNext(this.currentIndex + 1);
     }
   }
 
--- src/app/utils/number.ts
+++ src/app/utils/number.ts
@@ -5,3 +5,8 @@ export function limitNumberInRange(val: number, min: number, max: number): numbe
 export function getPercent(min: number, max: number, val: number) {
     return (val - min) / (max - min) * 100;
 }
+
+// 取[min, max]之间的一个随机数
+export function getRandomInt(range: [number, number]): number {
+    return Math.floor(Math.random() * (range[1] - range[0] + 1) + range[0]);
+}
